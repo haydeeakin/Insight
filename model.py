3c8350fe29388b4b45875e052f1495bfc43a4cf9
haydeeakin <50877984+haydeeakin@users.noreply.github.com>
2019-06-26 14:55:02 -0400

    Update model.py

--- model.py
+++ model.py
@@ -8,3 +8,19 @@ import seaborn as sns
 sns.set(style="white")
 sns.set(style="whitegrid", color_codes=True)
 data = pd.read_csv('....csv')
+data.head()
+target=data.filter(items=['target1'])
+target.columns = ['Target']
+target.head()
+
+df=data.filter(items=['Nominated','Index Director Nomination'])
+Xtrain, Xtest, ytrain, ytest = train_test_split(df, target, test_size=0.30,random_state=42)
+dflr=LogisticRegression(random_state=43,class_weight='balanced', solver='liblinear')
+fit_lr=dflr.fit(Xtrain, ytrain.values.ravel())
+predicted_lr=fit_lr.predict(Xtest)
+acc_log=accuracy_score(predicted_lr, ytest)
+acc_log
+0.8276611151339609
+correctpredic=df['']
+dflr.coef_
+array([[ 1.89187706, -5.15593322]])
