2d8cba7bd4bb35c163c30edbc6e9f59db6d54ff3
haydeeakin <50877984+haydeeakin@users.noreply.github.com>
2019-06-26 15:04:14 -0400

    Update model.py

--- model.py
+++ model.py
@@ -12,7 +12,6 @@ data.head()
 target=data.filter(items=['target1'])
 target.columns = ['Target']
 target.head()
-
 df=data.filter(items=['Nominated','Index Director Nomination'])
 Xtrain, Xtest, ytrain, ytest = train_test_split(df, target, test_size=0.30,random_state=42)
 dflr=LogisticRegression(random_state=43,class_weight='balanced', solver='liblinear')
@@ -20,14 +19,8 @@ fit_lr=dflr.fit(Xtrain, ytrain.values.ravel())
 predicted_lr=fit_lr.predict(Xtest)
 acc_log=accuracy_score(predicted_lr, ytest)
 acc_log
-
 correctpredic=df['']
 dflr.coef_
-
 corrcd = countdata.corr()
 corrcd
-
-%config InlineBackend.figure_format='svg'
-sns.lmplot(x='Index Director Nomination', y='target1', data=data, logistic=True, y_jitter=.03,hue='target1', )
-plt.savefig('target.png',dpi=300,bbox_inches="tight")
 
